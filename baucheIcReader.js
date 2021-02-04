2ebe2b516c6d31e4cbddea86d14e47e99adbbab2
jianzi <jianzi0307@icloud.com>
2021-02-04 12:48:26 +0800

    fix

--- baucheIcReader.js
+++ baucheIcReader.js
@@ -70,9 +70,10 @@ class BaucheIcReader {
   listenEvents() {
     // ws连接成功
     this.ws.onopen = (event) => {
+      console.log('ws connected.')
       this.startIc()
     }
-    this.ws.onmessage = this.onMessage
+    this.ws.onmessage = (event) => { this.onMessage(event) }
     // ws连接异常需要重连
     this.ws.onerror = (event) => {
       console.log('websocket connect error.')
@@ -80,7 +81,8 @@ class BaucheIcReader {
     }
     // 被动关闭
     this.ws.onclose = (event) => {
-      this.wsDisconnect()
+      console.log('connection is lost.')
+      this.wsReconnect()
     }
   }
 
@@ -90,8 +92,12 @@ class BaucheIcReader {
     if (aDataArray.length === 0) return
     // 解构
     const [byte0, byte1, byte2, byte3, byte4, byte5] = aDataArray
+    console.log(`received message type:${byte1} hex:${byte1.toString(16)}`);
     if (byte0 != 0xef) return
     switch (byte1) {
+      // case 0x20:
+      //   this.startAutoReadIc()
+      //   break;
       // 身份证信息
       case 0x23:
         // byte4: len byte3: type
@@ -99,7 +105,8 @@ class BaucheIcReader {
         for (let i = 0; i < byte4; i++) {
           data[i] = aDataArray[5 + i]
         }
-        this.handleMessage(byte3, decodeURIComponent(byteToString(data)))
+        const rs = this.byteToString(data)
+        this.handleMessage(byte3, decodeURIComponent(rs))
         break;
       // 身份证照片
       case 0x3e:
@@ -108,24 +115,15 @@ class BaucheIcReader {
         for (let i = 0; i < len; i++) {
           baseDataArray[i] = aDataArray[6 + i]
         }
-        const str = byteToString(baseDataArray)
+        const str = this.byteToString(baseDataArray)
         this.handleMessage(19, decodeURIComponent(str))
         break;
-      // case 0x71:
-      //   const len = byte4 * 256 + byte5
-      //   const data = new Uint8Array(len)
-      //   for (let i = 0; i < len; i++) {
-      //     data[i] = aDataArray[6 + i]
-      //   }
-      //   const str = byteToString(data)
-      //   const text = decodeURIComponent(str)
-      //   this.handleMessage(byte3, text)
-      //   break;
     }
   }
 
   // 简单的重连机制: 没连接上会一直重连
-  wsReconnect() {
+  wsReconnect(force = false) {
+    if (force) this.reconnecting = false
     if (this.reconnecting) return
     this.reconnecting = true
     this.timer = setTimeout(() => {
@@ -138,10 +136,19 @@ class BaucheIcReader {
 
   // ws主动断开连接
   wsDisconnect() {
-    if (this.ws.readyState === 1) {
+    if (this.ws && this.ws.readyState === 1) {
       this.ws.close()
       this.ws = null
     }
+  }
+
+  // 发送消息
+  wsSendMessage(buffer) {
+    if (!this.ws || this.ws.readyState !== 1) {
+      // this.wsReconnect(true)
+      return
+    }
+    this.ws.send(buffer)
   }
 
   // 处理身份证信息
@@ -151,16 +158,15 @@ class BaucheIcReader {
       this.idCard = {}
     }
     this.idCard[key] = msg
+    console.log('idCard===>', type, ':', msg)
   }
 
   // 启动身份证模块功能
   startIc() {
     if (this.isIE) {
       axCam_Ocx.StartIDCard()
-      this.autoMode && this.startAutoReadIc()
-    } else {
-      this.autoMode && this.wsSendMsgStartIC()
     }
+    this.autoMode && this.startAutoReadIc()
   }
 
   // WS方式身份证模块功能自动读卡
@@ -170,7 +176,7 @@ class BaucheIcReader {
     aDataArray[1] = 0x3c
     aDataArray[2] = 0x01
     aDataArray[3] = 0x00
-    this.ws.send(aDataArray.buffer)
+    this.wsSendMessage(aDataArray.buffer)
   }
 
   // WS方式手动启动身份证模块功能
@@ -180,7 +186,7 @@ class BaucheIcReader {
     aDataArray[1] = 0x3c;
     aDataArray[2] = 0x01;
     aDataArray[3] = 0x01;
-    this.ws.send(aDataArray.buffer);
+    this.wsSendMessage(aDataArray.buffer);
   }
 
   // WS方式关闭身份证自动读卡
@@ -190,7 +196,7 @@ class BaucheIcReader {
     aDataArray[1] = 0x41;
     aDataArray[2] = 0x01;
     aDataArray[3] = 0x00;
-    this.ws.send(aDataArray.buffer);
+    this.wsSendMessage(aDataArray.buffer);
   }
 
   // WS方式读取身份证信息
@@ -200,7 +206,7 @@ class BaucheIcReader {
     aDataArray[1] = 0x3d;
     aDataArray[2] = 0x01;
     aDataArray[3] = type;
-    this.ws.send(aDataArray.buffer);
+    this.wsSendMessage(aDataArray.buffer);
   }
 
   // 启动身份证自动读卡
@@ -294,11 +300,64 @@ class BaucheIcReader {
     }
   }
 
+  stringToByte(str) {
+    const bytes = new Array()
+    let len, c
+    len = str.length
+    for (let i = 0; i < len; i++) {
+      c = str.charCodeAt(i)
+      if (c >= 0x010000 && c <= 0x10FFFF) {
+        bytes.push(((c >> 18) & 0x07) | 0xF0)
+        bytes.push(((c >> 12) & 0x3F) | 0x80)
+        bytes.push(((c >> 6) & 0x3F) | 0x80)
+        bytes.push((c & 0x3F) | 0x80)
+      } else if (c >= 0x000800 && c <= 0x00FFFF) {
+        bytes.push(((c >> 12) & 0x0F) | 0xE0)
+        bytes.push(((c >> 6) & 0x3F) | 0x80)
+        bytes.push((c & 0x3F) | 0x80)
+      } else if (c >= 0x000080 && c <= 0x0007FF) {
+        bytes.push(((c >> 6) & 0x1F) | 0xC0)
+        bytes.push((c & 0x3F) | 0x80)
+      } else {
+        bytes.push(c & 0xFF)
+      }
+    }
+    return bytes
+  }
+
+  // 字节数组转字符串
+  byteToString(arr) {
+    if (typeof arr === 'string') {
+      return arr
+    }
+    let str = '',
+      _arr = arr
+    for (let i = 0; i < _arr.length; i++) {
+      const one = _arr[i].toString(2),
+        v = one.match(/^1+?(?=0)/)
+      if (v && one.length == 8) {
+        const bytesLength = v[0].length
+        const store = _arr[i].toString(2).slice(7 - bytesLength)
+        for (let st = 1; st < bytesLength; st++) {
+          store += _arr[st + i].toString(2).slice(2)
+        }
+        str += String.fromCharCode(parseInt(store, 2))
+        i += bytesLength - 1
+      } else {
+        str += String.fromCharCode(_arr[i])
+      }
+    }
+    return str
+  }
+
   // 获取身份证信息
   getIcInfo() {
-    return this.idCard
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve(this.idCard)
+      }, 1000)
+    })
   }
 }
 
 export default BaucheIcReader
-
